54860ef0d9675752c41c07437703127ffe8a3dba
Alejandro Batres <ralexbatres@gmail.com>
2023-07-19 13:25:52 -0600

    fix: moved HOC from _app

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from "next"
 
+import { GameCollectionProvider } from "../src/app/gameCollection/controller/gameCollectionController";
 import CollectionPage from "../src/app/gameCollection/view/pages/Collection";
 
 const Collection: NextPage = () => {
     return (
-        <CollectionPage />
+        <GameCollectionProvider>
+            <CollectionPage />
+        </GameCollectionProvider>
     )
 }
 
