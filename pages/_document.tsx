73f42438454bb7440053accaadc44c8ead23dbd6
ComplexRalex <ralexbatres@gmail.com>
2026-03-15 15:47:43 -0600

    feat: added manifest.json, tags and icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 function Document() {
     return (
-        <Html lang="en" data-bs-theme="dark">
+        <Html lang="en">
             <Head>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
-                <link rel="icon" href="./favicon.ico" />
+                <link rel="manifest" href="/backlog-games/manifest.json" />
+                <link rel="icon" href="/backlog-games/favicon.ico" />
+                <meta name="theme-color" content="#3c3e4e" />
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <link rel="apple-touch-icon" href="/backlog-games/icons/icon-192.svg" />
             </Head>
             <body data-bs-theme="dark">
                 <Main />
