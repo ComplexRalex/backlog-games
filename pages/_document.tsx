d573f0caab4f8b646d4235f31e84e6f7942ef9e7
ComplexRalex <ralexbatres@gmail.com>
2026-03-15 16:10:40 -0600

    fix: color theme not being applied properly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 function Document() {
     return (
-        <Html lang="en">
+        <Html lang="en" data-bs-theme="dark">
             <Head>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link rel="manifest" href="/backlog-games/manifest.json" />
                 <link rel="icon" href="/backlog-games/favicon.ico" />
+                <meta name="color-scheme" content="dark light" />
                 <meta name="theme-color" content="#3c3e4e" />
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <link rel="apple-touch-icon" href="/backlog-games/icons/icon-192.svg" />
             </Head>
-            <body data-bs-theme="dark">
+            <body>
                 <Main />
                 <NextScript />
             </body>
