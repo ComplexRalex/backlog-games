11c8e20f7b38a878a338f0a30ee02b01008ce455
ComplexRalex <ralexbatres@gmail.com>
2026-03-15 15:45:56 -0600

    fix: moved link tags into Head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 function Document() {
     return (
         <Html lang="en" data-bs-theme="dark">
-            <Head />
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
-            <link rel="icon" href="./favicon.ico" />
-            <body>
+            <Head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+                <link rel="icon" href="./favicon.ico" />
+            </Head>
+            <body data-bs-theme="dark">
                 <Main />
                 <NextScript />
             </body>
